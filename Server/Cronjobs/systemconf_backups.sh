f715f8218c87800a7d331cfa638ebc92a5aa7155
Adam Schoonover <adam@elchert.net>
2016-07-19 22:17:18 -0400

    updates 07-19-2016

--- Server/Cronjobs/systemconf_backups.sh
+++ Server/Cronjobs/systemconf_backups.sh
@@ -11,27 +11,47 @@ dbDirectory="/home/adamschoonover/Dropbox/Logs"
 
 haproxyCFG=$(md5sum /etc/haproxy/haproxy.cfg | awk '{print $1;}')
 haproxyBACKUP=$(md5sum /home/adamschoonover/Git/Personal/Backups/Haproxy/haproxy.cfg | awk '{print $1;}')
- 
+
+crontabBackupHash=$(md5sum $DIR/Cron/server_root_crontab | awk '{print $1;}')
+crontabHash=$(crontab -u root -l | md5sum | awk '{print $1;}')
+
+bashrcLocation="/home/adamschoonover/.bashrc"
+bashrcBackup="/home/adamschoonover/Git/Personal/Backups/Bash/server_bashrc" 
+
+counter=0
+
 git_add() {
   cd /home/adamschoonover/Git/Personal/
   git add -A .
   git commit -m "updates $NOW"
   git push
-
 }
 
 
+# CHECK IF HAPROXY IS BACKED UP
 if [[ $haproxyCFG != $haproxyBACKUP ]]; then
     cp /etc/haproxy/haproxy.cfg $DIR/Haproxy/
-
     printf "\n Updated HAPROXY conf - $NOW\n" >> $dbDirectory/systemconf_backups.txt
-
-    git_add
+    counter+=1
 fi
 
-# Creates a .crontab backup in tmp directory
-crontab -u root -l > $DIR/Cron/server_root_crontab
+#checks crontab
+if [[ $crontabBackupHash != $crontabHash ]]; then
+	crontab -u root -l > $DIR/Cron/server_root_crontab
+   printf "\n Updated Crontab conf - $now\n" >> $dbDirectory/systemconf_backups.txt
 
-cp /home/adamschoonover/.bashrc $DIR/Bash/server_bashrc
+   counter+=1 
+fi
 
-git_add
+#checks if bashrc is backed up
+if [[ $(cmp -s $bashrcLocation $bashrcBackup) == 1 ]]; then
+	cp $bashrcLocation $bashrcBackup
+	printf "\n Updated Bashrc backup - $NOW\n" >> $dbDirectory/systemconf.backups.txt
+   counter+=1
+fi
+
+if [[ $counter -ge 1 ]]; then
+	git_add
+else
+	echo "No updates"
+fi
