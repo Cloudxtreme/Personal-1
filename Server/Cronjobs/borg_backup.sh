3f5a1a8700ba1afa277e372c83bd39237f71cdbb
Adam Schoonover <aschoonover@sevone.com>
2016-07-19 20:09:00 -0400

    borg update

--- Server/Cronjobs/borg_backup.sh
+++ Server/Cronjobs/borg_backup.sh
@@ -1,16 +1,31 @@
 #!/bin/sh
 REPOSITORY="/mnt/Backups"
+NOW=$(date +"%m-%d-%Y")
+FILE=$(find /home/adamschoonover/Dropbox/Logs/ -iname borg*)
+EMAIL='adam@elchert.net'
+lastBorgBackup=$(borg list /mnt/Backups/ | awk '{print $1;}' | tail -n 1)
 
 # Backup all of /home and /var/www except a few
 # excluded directories
 borg create -v --stats                          \
     $REPOSITORY::`hostname`-`date +%Y-%m-%d`    \
-    /mnt/NAS                                                                         \
+    /mnt/NAS
 
+borg info /mnt/Backups::$lastBorgBackup >> $FILE
 
 # Use the `prune` subcommand to maintain 7 daily, 4 weekly and 6 monthly
 # archives of THIS machine. --prefix `hostname`- is very important to
 # limit prune's operation to this machine's archives and not apply to
 # other machine's archives also.
-borg prune -v $REPOSITORY --prefix `hostname`- \
-    --keep-daily=7 --keep-weekly=4 --keep-monthly=6
+
+echo "" >> $FILE
+
+borg prune --stats -v $REPOSITORY --prefix `hostname`- \
+    --keep-daily=7 --keep-weekly=4 --keep-monthly=6 >> $FILE
+
+###########
+# Email Log
+###########
+
+cat $FILE | mail -s "Borg Backup - $NOW" $EMAIL
+printf "\n ##### EMAILED $NOW - $EMAIL ######\n" >> $File
