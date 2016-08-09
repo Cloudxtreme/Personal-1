917c3524ec1183bd6fc4afab08c95bd91ca1ed40
Adam Schoonover <adam@elchert.net>
2016-08-09 07:00:07 -0400

    usenet backup update - 08-09-2016

--- Server/Cronjobs/public-ip-check.sh
+++ Server/Cronjobs/public-ip-check.sh
@@ -12,17 +12,17 @@ echo $NOW "==>" $IP >> $filePath
 # run function
 getIP
 
-ownerName=$(stat -c %U /home/adamschoonover/Git/Personal/Server/Cronjobs/public-ip-check.sh)
-
-if [ "$ownerName" != "adamschoonover" ]; then
-	chown adamschoonover $filePath | tail -n 1
-else
-	exit 0
-fi
-
 if [ $(cat $filePath | wc -l) > 20 ]; then
 	printf "#######\n\n Public IP Address\n\n#######\n\n" > $filePath
 	getIP
 else
 	exit 0
+fi
+
+ownerName=$(stat -c %U /home/adamschoonover/Git/Personal/Server/Cronjobs/public-ip-check.sh)
+
+if [ "$ownerName" != "adamschoonover" ]; then
+        chown adamschoonover $filePath | tail -n 1
+else
+        exit 0
 fi
