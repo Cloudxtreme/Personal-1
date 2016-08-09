194a73671b0bce72021e4b9d3edf90a029ff6ff4
Adam Schoonover <adam@elchert.net>
2016-08-08 22:23:37 -0400

    updates

--- Server/Cronjobs/public-ip-check.sh
+++ Server/Cronjobs/public-ip-check.sh
@@ -2,7 +2,27 @@
 # Get's the public IP and writes it /Dropbox/Logs/
 
 NOW=$(date +"%m-%d-%Y")
-
+filePath="/home/adamschoonover/Dropbox/Logs/server_public_ip.txt"
 IP=$(curl 'http://myexternalip.com/raw')
 
-echo $NOW "==>" $IP >> /home/adamschoonover/Dropbox/Logs/server_public_ip.txt
+getIP (){
+echo $NOW "==>" $IP >> $filePath
+}
+
+# run function
+getIP
+
+ownerName=$(stat -c %U /home/adamschoonover/Git/Personal/Server/Cronjobs/public-ip-check.sh)
+
+if [ "$ownerName" != "adamschoonover" ]; then
+	chown adamschoonover $filePath | tail -n 1
+else
+	exit 0
+fi
+
+if [ $(cat $filePath | wc -l) > 20 ]; then
+	printf "#######\n\n Public IP Address\n\n#######\n\n" > $filePath
+	getIP
+else
+	exit 0
+fi
