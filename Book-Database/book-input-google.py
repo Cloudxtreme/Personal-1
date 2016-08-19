74e167eecbb0745a94078037636729cb0f32885c
Adam Schoonover <aschoonover@sevone.com>
2016-08-19 09:04:54 -0400

    updates

--- Book-Database/book-input-google.py
+++ Book-Database/book-input-google.py
@@ -1,12 +1,7 @@
-#Pull page numbers via API
-#http://isbndb.com/api/v2/docs/books
-# API KEY HD45DAPU
+# Enter in an ISBN and get book information via Google books
+# and enter it into a local mysql database
 
-import urllib2
-import json
-import mysql.connector
-import time
-import os
+import urllib2 json mysql.connector time os
 
 apikey = "AIzaSyDozlghDCtPzYu9ckvt1jhkjinZpM3vhJQ"
 
