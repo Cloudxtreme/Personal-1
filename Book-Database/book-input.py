6c8ab8049e02830b2ee10fbf18e2fedeab1db4d8
Adam Schoonover <adam@elchert.net>
2016-10-25 20:41:33 -0400

    updates

--- Book-Database/book-input.py
+++ Book-Database/book-input.py
@@ -1,115 +1,108 @@
-# Enter in an ISBN and get book information via Google books
+# Enter in an Isbn and get book information via Google books
 # and enter it into a local mysql database
 
 import urllib2, json, mysql.connector, time, os
 
 apikey = "AIzaSyDozlghDCtPzYu9ckvt1jhkjinZpM3vhJQ"
-cnx = mysql.connector.connect(user='root', database='booksread', buffered=True)
-cursor = cnx.cursor()
-url = 'https://www.googleapis.com/books/v1/volumes?q=isbn:'
+#cnx = mysql.connector.connect(user='root', database='booksread', buffered=True)
+#cursor = cnx.cursor()
+url = 'https://www.googleapis.com/books/v1/volumes?q=Isbn:'
 
-def enter_book_data(ISBN,Rating,NOTES):
-    #ISBN = raw_input('Enter ISBN: ')
-    final_url = url + ISBN
+class book_data():
 
-    #open url and make a variable
-    json_obj = urllib2.urlopen(final_url)
+        def __init__(self, Isbn):
+            self.Isbn = Isbn
 
-    #parse the url data with json
-    jsonData = json.load(json_obj)
+        def grab_json(self, Isbn):
 
-    #input pulled from json
-    Name = jsonData['items'][0]['volumeInfo']['title']
-    AuthorName = jsonData['items'][0]['volumeInfo']['authors'][0]
-    DateFinished = time.strftime("%Y-%m-%d")
-    Pages = jsonData['items'][0]['volumeInfo']['pageCount']
+            final_url = url + Isbn
+            json_obj = urllib2.urlopen(final_url)
+            jsonData = json.load(json_obj)
+            return jsonData
 
+        def check_for_book(self, Isbn):
 
-    book_data = {
-      'Name': Name,
-      'AuthorName': AuthorName,
-      'Rating': Rating,
-      'DateFinished': DateFinished,
-      'Pages': Pages,
-      'ISBN': ISBN,
-      'NOTES': NOTES,
-    }
+            if self.grab_json(Isbn)['totalItems'] == 0:
+                return False
+            else:
+                # entry exsists on google books
+                return True
 
-    add_book = ("INSERT INTO booksread "
-                "(Name,AuthorName,Rating,DateFinished,Pages,ISBN,NOTES) "
-                "VALUES (%(Name)s, %(AuthorName)s, %(Rating)s, %(DateFinished)s, %(Pages)s, %(ISBN)s, %(NOTES)s)")
+        def Isbn_false(self, Isbn):
+            print "\n Google Books does not have this book :( \n"
+            # Google books doesn't have the book, so all values must
+            # be manually put in
+            Name = raw_input("Name: ")
+            AuthorName = raw_input("Author's Name: ")
+            DateFinished = raw_input("Date Finished (year-month-day): ")
+            Pages = raw_input("Pages: ")
+            Rating = raw_input("raw_input: ")
+            Notes = raw_input("Notes: ")
 
-    #enter the data into the database
-    cursor.execute(add_book, book_data)
-    cnx.commit()
-    cursor.close()
-    cnx.close()
+            book_data = {
+                'Name': Name,
+                'AuthorName': AuthorName,
+                'Rating': Rating,
+                'DateFinished': DateFinished,
+                'Pages': Pages,
+                'Isbn': Isbn,
+                'Notes': Notes }
 
+            return book_data
 
+        def Isbn_true(self, Isbn):
 
-    print "Name: " + Name
-    print "AuthorName: " + AuthorName
-    print "Date Finished: " + DateFinished
-    print "Pages of book: %s" % Pages
+            #all the json data from google books
+            jsonData = self.grab_json(Isbn)
 
-    print "\nInformation entered into the database.\n\n"
+            #Pulling information for the dict from the json
+            Name = jsonData['items'][0]['volumeInfo']['title']
+            AuthorName = jsonData['items'][0]['volumeInfo']['authors'][0]
+            DateFinished = time.strftime("%Y-%m-%d")
+            Pages = jsonData['items'][0]['volumeInfo']['pageCount']
+            Rating = raw_input("Rating: ")
+            Notes = raw_input("Notes: ")
+
+            #A dict of the information above
+            book_data = {
+                 'Name': Name,
+                 'AuthorName': AuthorName,
+                 'Rating': Rating,
+                 'DateFinished': DateFinished,
+                 'Pages': Pages,
+                 'Isbn': Isbn,
+                 'Notes': Notes,
+                 }
+
+            return book_data
+
+        def insert_into_database(self, Isbn):
+
+            if self.check_for_book(Isbn) == True:
+                book_dict = self.Isbn_true(Isbn)
+            else:
+                book_dict = self.Isbn_false(Isbn)
+
+            cnx = mysql.connector.connect(user='root', database='booksread', buffered=True)
+            cursor = cnx.cursor()
+
+            #enter the data into the database
+            add_book = ("INSERT INTO booksread "
+                             "(Name,AuthorName,Rating,DateFinished,Pages,Isbn,Notes) "
+                             "VALUES (%(Name)s, %(AuthorName)s, %(Rating)s, %(DateFinished)s, %(Pages)s, %(Isbn)s, %(Notes)s)")
+            cursor.execute(add_book, book_dict)
+
+            cnx.commit()
+            cursor.close()
+            cnx.close()
 
 def main():
-
     print "#################\n"
     print "Book Database\n"
     print "#################\n\n"
 
-    # User input for books
-    ISBN = raw_input("Enter ISBN: ")
-    Rating = raw_input("Rating: ")
-    NOTES = raw_input("Notes: ")
+    Isbn = raw_input("Isbn: ")
+    books = book_data(Isbn)
+    books.insert_into_database(Isbn)
 
-    enter_book_data(ISBN, Rating, NOTES)
-
-def notinDatabase():
-    print "Unfortunately you'll have to enter a few things again..\n\n"
-
-    ISBN = raw_input("Enter ISBN: ")
-    AuthorName = raw_input("Enter Author's Name(First Name, Last Name): ")
-    Name = raw_input("Enter Name: ")
-    Rating = raw_input("Rating: ")
-    NOTES = raw_input("Notes: ")
-    DateFinished = time.strftime("%Y-%m-%d")
-    Pages = raw_input("Pages: ")
-
-    book_data = {
-      'Name': Name,
-      'AuthorName': AuthorName,
-      'Rating': Rating,
-      'DateFinished': DateFinished,
-      'Pages': Pages,
-      'ISBN': ISBN,
-      'NOTES': NOTES,
-    }
-
-    add_book = ("INSERT INTO booksread "
-                "(Name,AuthorName,Rating,DateFinished,Pages,ISBN,NOTES) "
-                "VALUES (%(Name)s, %(AuthorName)s, %(Rating)s, %(DateFinished)s, %(Pages)s, %(ISBN)s, %(NOTES)s)")
-
-    #enter the data into the database
-    cursor.execute(add_book, book_data)
-    cnx.commit()
-    cursor.close()
-    cnx.close()
-
-    print "#################\n"
-    print "Book Database\n"
-    print "#################\n\n"
-
-    print "Name: " + Name
-    print "AuthorName: " + AuthorName
-    print "Date Finished: " + DateFinished
-    print "Pages of book: %s" % Pages
-
-    print "\nInformation entered into the database.\n\n"
-
-try:
-    main()
-except:
-    notinDatabase()
+main()
