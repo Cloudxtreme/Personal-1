a145fe6bf80aa9ed2bdff5903d1deac030fe3f1a
Adam Schoonover <adam@elchert.net>
2016-11-22 22:06:34 -0500

    updates

--- Server/Cronjobs/systemconf_backups.sh
+++ Server/Cronjobs/systemconf_backups.sh
@@ -6,7 +6,7 @@
 
 NOW=$(date +"%m-%d-%Y")
 DIR='/home/adamschoonover/Git/Personal/Backups'
-logFile="/home/adamschoonover/Dropbox/Logs/config_backups.txt"
+logFile="/home/adamschoonover/Dropbox/Logs/systemconf_backups.txt"
 dbDirectory="/home/adamschoonover/Dropbox/Logs"
 
 haproxyCFG=$(md5sum /etc/haproxy/haproxy.cfg | awk '{print $1;}')
@@ -19,13 +19,10 @@ bashrcHash=$(md5sum /home/adamschoonover/.bashrc | awk '{print $1;}')
 bashrcBackupHash=$(md5sum /home/adamschoonover/Git/Personal/Backups/Bash/server_bashrc | awk '{print $1;}')
 
 counter=0
-
-check_file_length(){
-	fileLength=$(cat $logFile | wc -l)
-	if [[$fileLength >= "50"]]; then
-        	echo "" > $logFile
-	fi
-}
+fileLength=$(cat $logFile | wc -l)
+if [ "$fileLength" -gt 100 ]; then
+	echo "" > $logFile
+fi
 
 git_add() {
   cd /home/adamschoonover/Git/Personal/
@@ -34,19 +31,17 @@ git_add() {
   git push
 }
 
-check_file_length
-
 # CHECK IF HAPROXY IS BACKED UP
 if [[ $haproxyCFG != $haproxyBACKUP ]]; then
-    cp /etc/haproxy/haproxy.cfg $DIR/Haproxy/
-    printf "\n Updated HAPROXY conf - $NOW\n" >> $dbDirectory/systemconf_backups.txt
-    counter+=1
+	cp /etc/haproxy/haproxy.cfg $DIR/Haproxy/
+    	printf "\n Updated HAPROXY conf - $NOW\n" >> $dbDirectory/systemconf_backups.txt
+    	counter+=1
 fi
 
 #checks crontab
 if [[ $crontabBackupHash != $crontabHash ]]; then
 	crontab -u root -l > $DIR/Cron/server_root_crontab
-   printf "\n Updated Crontab conf - $NOW\n" >> $dbDirectory/systemconf_backups.txt
+	printf "\n Updated Crontab conf - $NOW\n" >> $dbDirectory/systemconf_backups.txt
    counter+=1
 fi
 
@@ -54,7 +49,7 @@ fi
 if [[ $bashrcHash != $bashrcBackupHash ]]; then
 	cp /home/adamschoonover/.bashrc $DIR/Bash/server_bashrc
 	printf "\n Updated Bashrc - $NOW\n" >> $dbDirectory/systemconf_backups.txt
-   counter+=1
+   	counter+=1
 fi
 
 ######
