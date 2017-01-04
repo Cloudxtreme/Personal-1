a250eb537780d57c11c52b94ddd5f4b41af38d2c
Adam Schoonover <aschoonover@sevone.com>
2017-01-03 19:00:35 -0500

    updates

--- Scripts/imports/bash/SevOne-version-number.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-# Use this to pull the version numbers
-
-base=$(cat /SevOne.info | awk '/base/{print $3}');
-major=$(cat /SevOne.info | awk '/major/{print $3}');
-minor=$(cat /SevOne.info | awk '/minor/{print $3}');
-patch=$(cat /SevOne.info | awk '/patch/{print $3}');
