ff67a8553736bedb1d6c421b93b66a487fba1d00
Adam Schoonover <aschoonover@sevone.com>
2017-02-14 23:07:42 -0500

    updates

---
+++ Server/Cronjobs/hdsentinelParsed.py
@@ -0,0 +1,25 @@
+import os
+
+removeLines = [
+    'HDD Model ID',
+    'HDD Serial No',
+    'HDD Revision',
+    'Interface',
+    'Power on time',
+    'Unknown  C',
+    'Unknown %'
+    ]
+
+oldFileLocation = '/tmp/hds.txt'
+newFileLocation = '/home/aelchert/Dropbox/Logs/hardDriveReport.txt'
+
+
+# create report from HDSentienl and put in temp folder
+os.system('sudo HDSentinel > ~/tmp/hds.txt')
+
+# parse temp file for lines not needed and write new file without them
+with open(oldFileLocation, 'r') as raw:
+    with open(newFileLocation, 'w') as hds:
+        for line in raw:
+            if not any(words in line for words in removeLines):
+                hds.write(line)
