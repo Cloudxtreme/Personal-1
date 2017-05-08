00b26a5de80868916e660cbb3db1d5521d7616be
Adam Schoonover <adamschoonover@Elchert-Family-MBP.local>
2017-05-08 19:51:39 -0400

    updates

--- Server/Cronjobs/mysqlBackup.py
+++ Server/Cronjobs/mysqlBackup.py
@@ -5,5 +5,4 @@ date = time.strftime("%m-%d-%y")
 directory = "/home/aelchert/Dropbox/Backup"
 EMAIL = '7ac1a19215fbf24b575197605f2ae1f8f5fef8ea@api.prowlapp.com'
 
-os.popen("mysqldump -u root -p'CuIeyy7j!!' Booksread > {}/{}.sql").format(directory,date)
-
+os.popen("mysqldump -u root -p'CuIeyy7j!!' Booksread > %s/%s.sql") % (directory, date)
