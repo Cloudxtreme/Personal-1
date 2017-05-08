019579f7dd5650ae77faf811dfde6edad524e74a
Adam Schoonover <aschoonover@sevone.com>
2017-05-08 19:07:28 -0400

    updates

--- Book-Database/bookUpdate.py
+++ Book-Database/bookUpdate.py
@@ -2,12 +2,10 @@
 # and enter it into a local mysql database
 
 import pymysql.cursors, json
-import urllib2, pprint
+import urllib2
 
-apikey = "AIzaSyDozlghDCtPzYu9ckvt1jhkjinZpM3vhJQ"
 url = 'https://www.googleapis.com/books/v1/volumes?q=Isbn:'
 
-bookISBN = ('0307278255')
 databaseISBNs = []
 
 def getBookData(Isbn):
@@ -26,37 +24,46 @@ connection = pymysql.connect(
     database='Booksread',
     host="nonstopflights.ddns.net",
     port=19234,
-    cursorclass=pymysql.cursors.DictCursor)
+    cursorclass=pymysql.cursors.DictCursor,
+    charset='utf8')
 
-try:
+with connection.cursor() as cursor:
+     # collect all of the ISBN
+     getIdCountQuery = ("select ISBN from booksread;")
+     cursor.execute(getIdCountQuery)
+     data = cursor.fetchall()
+
+     # add values to databaseISBNs
+     for i in data: databaseISBNs.append(i['ISBN'])
+     cursor.close()
+
+
+
+    # loop through databaseISBNs
+for bookISBN in databaseISBNs:
     with connection.cursor() as cursor:
-         # collect all of the ISBN
-         getIdCountQuery = ("select ISBN from booksread;")
-         cursor.execute(getIdCountQuery)
-         data = cursor.fetchall()
+        # using the ISBN, see if there is a summery already
+        summeryAvail = cursor.execute("select Summery from booksread where ISBN=(%s)",(bookISBN))
+        print summeryAvail
+        #print "bookISBN: {}".format(bookISBN)
+        #print "summeryAvail: {}".format(summeryAvail)
 
-         # add values to databaseISBNs
-         for i in data: databaseISBNs.append(i['ISBN'])
+        try:
+            bookData = getBookData(bookISBN)
+            if bookISBN is not " " and summeryAvail == 1:
+                bookDescription = bookData['items'][0]['volumeInfo']['description']
+                bookUpdate = [bookDescription,bookISBN]
+                #print bookDescription
+                updateQuery = "update booksread set Summery=%s where ISBN=%s"
+                print "Updated {}".format(bookISBN)
 
-         # loop through databaseISBNs
-         for bookISBNs in databaseISBNs:
+                cursor.execute(updateQuery,bookUpdate)
 
-            # using the ISBN, see if there is a summery already
-            summeryAvail = cursor.execute("select Summery from booksread where ISBN=%s",(bookISBNs))
+        except KeyError or UnicodeEncodeError:
+            print "Key Error, ISBN: {}. Error: {}".format(bookISBN, KeyError)
+            continue
 
-            # skip entry with no ISBN or already has a summery
-            if bookISBNs == "" or summeryAvail is not 1:
-                 pass
-            else:
-                 #working
-                 bookData = getBookData(bookISBN)
-                 bookDescription = bookData['items'][0]['volumeInfo']['description']
-                 bookUpdate = [bookDescription,bookISBNs]
-                 updateQuery = "update booksread set Summery=%s where ISBN=%s"
-                 print "Updated {}".format(bookISBNs)
 
-                 cursor.execute(updateQuery,bookUpdate)
 
-finally:
-    connection.commit()
-    connection.close()
+connection.commit()
+connection.close()
