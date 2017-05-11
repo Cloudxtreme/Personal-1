f976de69f6b0383707cab3ee7dceaa890a93a607
Adam Schoonover <aschoonover@sevone.com>
2017-05-11 00:55:38 -0400

    updates

--- Server/Cronjobs/systemconfBackups.sh
+++ Server/Cronjobs/systemconfBackups.sh
@@ -15,7 +15,7 @@ crontabBackupHash=$(md5sum $DIR/Cron/server_root_crontab | awk '{print $1;}')
 crontabHash=$(crontab -u root -l | md5sum | awk '{print $1;}')
 
 bashrcHash=$(md5sum /home/aelchert/.bashrc | awk '{print $1;}')
-bashrcBackupHash=$(md5sum /home/aelchert/Git/Personal/Backups/Bash/server_bashrc | awk '{print $1;}')
+bashrcBackupHash=$(md5sum $DIR/Bash/server_bashrc | awk '{print $1;}')
 
 counter=0
 
@@ -37,12 +37,15 @@ git_add() {
 }
 
 #empties log file after 100 lines
+printf "\n Checking File Length"
 checkFileLength
 
 # run python script for nginx conf
+printf "\n Running nginxBackup.py"
 python nginxBackup.py
 
 #checks crontab
+printf "\n Checking Crontab"
 if [[ $crontabBackupHash != $crontabHash ]]; then
 	crontab -u root -l > $DIR/Cron/server_root_crontab
 	printf "++ [systemconfBackups] - Crontab Backup - $NOW" >> $LOGFILE
@@ -50,6 +53,7 @@ if [[ $crontabBackupHash != $crontabHash ]]; then
 fi
 
 #checks if bashrc is backed up
+printf "\n Checking Bashrc"
 if [[ $bashrcHash != $bashrcBackupHash ]]; then
 	cp /home/aelchert/.bashrc $DIR/Bash/server_bashrc
 	printf "++ [systemconfBackups] - Bashrc Backup - $NOW" >> $LOGFILE
