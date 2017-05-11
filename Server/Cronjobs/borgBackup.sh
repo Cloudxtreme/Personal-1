951e1e9422d3a91e41b9c289ad4bdc4e5cd721e1
Adam Schoonover <adamschoonover@Elchert-Family-MBP.local>
2017-05-11 13:09:19 -0400

    updates

--- Server/Cronjobs/borgBackup.sh
+++ Server/Cronjobs/borgBackup.sh
@@ -5,22 +5,12 @@ LOGFILE="/home/aelchert/Dropbox/Logs/cronLog.txt"
 EMAIL='7ac1a19215fbf24b575197605f2ae1f8f5fef8ea@api.prowlapp.com'
 lastBorgBackup=$(sudo borg list /mnt/Backups/ | awk '{print $1;}' | tail -n 1)
 
-resetLog() {
-    echo "" > $LOGFILE
-}
-
-#clear the log
-resetLog
-
 # Backup all of /home and /var/www except a few
 # excluded directories
 sudo borg create -v --stats $REPOSITORY::`hostname`-`date +%Y-%m-%d` /mnt/NAS
 
 if [ $? -eq 0 ]; then
   echo "++ [borgBackup.sh] Completed $NOW" >> $LOGFILE
-else
-  echo "-- [borgBackup.sh] FAILED $NOW" >> $LOGFILE
-fi
 
 # Use the `prune` subcommand to maintain 7 daily, 4 weekly and 6 monthly
 # archives of THIS machine. --prefix `hostname`- is very important to
@@ -33,7 +23,7 @@ borg prune --stats -v $REPOSITORY --prefix `hostname`- \
     --keep-daily=7 --keep-weekly=4 >> $LOGFILE
 
 if [ $? -eq 0 ]; then
-  echo "++ [borgBackup.sh - Prune] Completed $NOW" >> $LOGFILE
+  echo "++ [borgBackup.sh] - Prune - Completed $NOW" >> $LOGFILE
 else
-  echo "-- [borgBackup.sh - Prune] FAILED $NOW" >> $LOGFILE
+  echo "-- [borgBackup.sh] - Prune - FAILED $NOW" >> $LOGFILE
 fi
