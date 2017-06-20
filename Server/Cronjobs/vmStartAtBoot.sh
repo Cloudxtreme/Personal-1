7b3a5c9785c5d18018751a3a2e2c9d10b2f445aa
Adam Schoonover <aschoonover@sevone.com>
2017-06-19 23:45:08 -0400

    updates

--- Server/Cronjobs/vmStartAtBoot.sh
+++ Server/Cronjobs/vmStartAtBoot.sh
@@ -4,7 +4,13 @@ logFile='/home/aelchert/Dropbox/Logs/cronLog.txt'
 
 # get list of VMs on the box and put into an array
 declare -a getVMsList
-getVMsList=($(VBoxManage list vms | cut -d ' ' -f1 | tr -d '"'))
+
+getVMsList=( \
+e2fe39a5-7bce-49ca-8891-3d61c35bef87 \
+021d36a2-e8b8-4594-9b6a-a55033339600 \
+2de22143-2b37-48db-b4dd-6237561083d3 \
+1d483b74-b0ee-436c-92d0-9a8ff2ff707a \
+)
 
 # Loop through VM name array and try to start them. If they are already running
 # it will return a 1. If it successfully starts them, it will return 0.
