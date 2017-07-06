cc5153ea0b92d3b8532e613743f6a26e64194bc0
Adam Schoonover <aschoonover@sevone.com>
2017-07-06 01:10:07 -0400

    updates

--- Server/Cronjobs/mysqlBackup.sh
+++ Server/Cronjobs/mysqlBackup.sh
@@ -4,12 +4,12 @@
 
 source '../../Resources/timeVariableNOW.sh'
 
-
+dateLog = $(date)
 DIR="/home/aelchert/Dropbox/Backup"
 LOGFILE="/home/aelchert/Dropbox/Logs/cronLog.txt"
 
 # dump Booksread database to Dropbox/Backup
-mysqldump -u root -p'CuIeyy7j!!' Booksread > $DIR/booksread_$(date).sql
+mysqldump -u root -p'CuIeyy7j!!' Booksread > $DIR/booksread_$LOGFILEDATE.sql
 
 if [ $? -eq 0 ]; then
   echo -e "++ [mysqlBackup.sh] - $NOW Completed" >> $LOGFILE
@@ -18,11 +18,11 @@ else
 fi
 
 # change owner of .sql backup file
-chown aelchert $DIR/booksread_$(date).sql
+chown aelchert $DIR/booksread_$LOGFILEDATE.sql
 
 #adds sql backups to tar file
 cd $DIR
-tar rvf Booksread_Backup.tar booksread_$(date).sql
+tar rvf Booksread_Backup.tar booksread_$LOGFILEDATE.sql
 
 if [ $? -eq 0 ]; then
   echo -e "++ [mysqlBackup.sh] - $NOW - Tar creation - Completed" >> $LOGFILE
@@ -30,7 +30,7 @@ else
   echo -e "-- [mysqlBackup.sh] - $NOW - Tar FAILED " >> $LOGFILE
 fi
 
-rm booksread_$NOW.sql
+rm booksread_$LOGFILEDATE.sql
 if [ $? -eq 0 ]; then
   echo -e "++ [mysqlBackup.sh] - $NOW - rm of Booksread.sql - Completed" >> $LOGFILE
 else
