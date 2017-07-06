9ed1426297a66836b8df5ba7b1a8fef761605453
Adam Schoonover <aschoonover@sevone.com>
2017-07-06 01:11:26 -0400

    updates

--- Server/Cronjobs/mysqlBackup.sh
+++ Server/Cronjobs/mysqlBackup.sh
@@ -12,27 +12,27 @@ LOGFILE="/home/aelchert/Dropbox/Logs/cronLog.txt"
 mysqldump -u root -p'CuIeyy7j!!' Booksread > $DIR/booksread_$LOGFILEDATE.sql
 
 if [ $? -eq 0 ]; then
-  echo -e "++ [mysqlBackup.sh] - $NOW Completed" >> $LOGFILE
+  echo -e "++ [mysqlBackup.sh] - $LOGFILEDATE Completed" >> $LOGFILE
 else
-  echo -e "-- [mysqlBackup.sh] - $NOW - FAILED" >> $LOGFILE
+  echo -e "-- [mysqlBackup.sh] - $LOGFILEDATE - FAILED" >> $LOGFILE
 fi
 
 # change owner of .sql backup file
-chown aelchert $DIR/booksread_$LOGFILEDATE.sql
+chown aelchert $DIR/booksread_$NOW.sql
 
 #adds sql backups to tar file
 cd $DIR
-tar rvf Booksread_Backup.tar booksread_$LOGFILEDATE.sql
+tar rvf Booksread_Backup.tar booksread_$NOW.sql
 
 if [ $? -eq 0 ]; then
-  echo -e "++ [mysqlBackup.sh] - $NOW - Tar creation - Completed" >> $LOGFILE
+  echo -e "++ [mysqlBackup.sh] - $LOGFILEDATE - Tar creation - Completed" >> $LOGFILE
 else
-  echo -e "-- [mysqlBackup.sh] - $NOW - Tar FAILED " >> $LOGFILE
+  echo -e "-- [mysqlBackup.sh] - $LOGFILEDATE - Tar FAILED " >> $LOGFILE
 fi
 
 rm booksread_$LOGFILEDATE.sql
 if [ $? -eq 0 ]; then
-  echo -e "++ [mysqlBackup.sh] - $NOW - rm of Booksread.sql - Completed" >> $LOGFILE
+  echo -e "++ [mysqlBackup.sh] - $LOGFILEDATE - rm of Booksread.sql - Completed" >> $LOGFILE
 else
-  echo -e "-- [mysqlBackup.sh] - $NOW - rm of Booksread.sql - FAILED " >> $LOGFILE
+  echo -e "-- [mysqlBackup.sh] - $LOGFILEDATE - rm of Booksread.sql - FAILED " >> $LOGFILE
 fi
