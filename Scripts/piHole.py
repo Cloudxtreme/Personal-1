9664757836082b750cc7381ff7e00729db9d1c0f
Adam Schoonover <adamschoonover@Elchert-Family-MBP.local>
2017-07-09 18:45:01 -0400

    updates

--- Scripts/piHole.py
+++ Scripts/piHole.py
@@ -1,7 +1,32 @@
-import requests,pprint
+import requests, pprint
 
 ip='10.0.0.49'
 url="http://{}/admin/api.php".format(ip)
-r = requests.get(url)
-data = r.json()
-pprint.pprint(data)
+
+def getApiData(url):
+    ''' Query piHole API for data '''
+    r = requests.get(url)
+    return r
+
+def makeDict():
+    r = getApiData(url)
+    data = r.json()
+
+    dataDict = {'ads_blocked_today': data['ads_blocked_today'],
+    'ads_percentage_today': data['ads_percentage_today'],
+    'dns_queries_today': data['dns_queries_today'],
+    'domains_being_blocked': data['domains_being_blocked'],
+    'queries_cached': data['queries_cached'],
+    'queries_forwarded': data['queries_forwarded'],
+    'unique_clients': data['unique_clients'],
+    'unique_domains': data['unique_domains']}
+
+    return dataDict
+
+for k,v in makeDict().items():
+    print(k,v)
+
+plugin_object_type=1581
+plugin_id=10
+indicator_type_id_domains_being_blocked = 12913
+indicator_type_id_dns_being_blocked = 12914
