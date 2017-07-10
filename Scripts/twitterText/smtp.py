27036b501b843e7e283cfcab1a2be6f3c4cab7d3
Adam Schoonover <adamschoonover@Elchert-Family-MBP.local>
2017-07-10 18:24:25 -0400

    updates

--- Scripts/twitterText/smtp.py
+++ Scripts/twitterText/smtp.py
@@ -1,5 +1,6 @@
 from sender import Message
 from sender import Mail
+from sender import Attachment
 
 
 # https://sender.readthedocs.io/
@@ -8,9 +9,11 @@ msg = Message("demo subject", fromaddr="adam@elchert.net",
               to="adam@elchert.net")
 
 msg.body = "demo body"
-msg.html = "<h1>Body test</h1>"
 
-mail = Mail("mail.messagingengine.com", port=465, username="aschoonover@fastmail.fm", password="MkaV3yYevpvKqbT2RarY",
+mail = Mail("mail.messagingengine.com",
+            port=465, username="aschoonover@fastmail.fm",
+            password="MkaV3yYevpvKqbT2RarY",
             use_tls=False, use_ssl=True, debug_level=None)
 
 mail.send(msg)
+msg.html.close()
