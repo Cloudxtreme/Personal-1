35c4f481bd53b0a207f55759609d93e0767f21d8
Adam Schoonover <aschoonover@sevone.com>
2017-07-10 17:50:08 -0400

    updates

---
+++ Scripts/twitterText/smtp.py
@@ -0,0 +1,13 @@
+from sender import Message
+from sender import Mail
+
+msg = Message("demo subject", fromaddr="adam@elchert.net",
+              to="adam@elchert.net")
+
+msg.body = "demo body"
+msg.html = "<h1>Body test</h1>"
+
+mail = Mail("mail.messagingengine.com", port=465, username="aschoonover@fastmail.fm", password="MkaV3yYevpvKqbT2RarY",
+            use_tls=False, use_ssl=True, debug_level=None)
+
+mail.send(msg)
