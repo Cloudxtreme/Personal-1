f4807140246eef6e67d300903650017d3e8555eb
Adam Schoonover <adamschoonover@Elchert-Family-MBP.local>
2017-07-25 21:44:16 -0400

    updates

--- Scripts/spotify/main.py
+++ Scripts/spotify/main.py
@@ -12,7 +12,7 @@ import sqlite3
 logger = logging.getLogger(__name__)
 log = logging.FileHandler('doucheSpotifyUpdate.log')
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-log.setLevel(logging.DEBUG)
+log.setLevel(logging.WARNING)
 log.setFormatter(formatter)
 logger.addHandler(log)
 ########################################################################
@@ -44,7 +44,8 @@ def createTables():
                          url TEXT,
                          artistName TEXT,
                          trackName TEXT,
-                         spotifyId TEXT
+                         spotifyId TEXT,
+                         newest INTEGER DEFAULT 0
                          )''')
             logger.info("Created new database tables.")
 
@@ -54,28 +55,39 @@ def createTables():
 
 def insertTrack(addedBy, addedAt, url, artistName, trackName, spotifyId):
     """ Insert into sqllite """
-    t = (addedBy, addedBy, url, artistName, trackName, spotifyId)
+    t = (addedBy, addedAt, url, artistName, trackName, spotifyId, 0)
     try:
         if conn is not None:
-            c.execute('INSERT INTO playlist VALUES (?, ?, ?, ?, ?, ?)', t)
+            c.execute('INSERT INTO playlist VALUES (?, ?, ?, ?, ?, ?, ?)', t)
+            logger.debug('Inserted ID: {}'.format(spotifyId))
     except:
         print(Exception)
         logger.error('Exception: %s', Exception)
     finally:
         conn.commit()
 
-###########################################################################
-''' Connect to Database '''
-conn = sqlite3.connect(dbName)
-c = conn.cursor()
-logger.debug('-- Logging into Database')
-###########################################################################
-''' Create database if not there '''
-createTables()
-logger.debug("Check for Database")
-###########################################################################
+def debug__pullJson(resultsObject):
+    #pprint.pprint(resultsObject)
+    for track in resultsObject['tracks']['items']:
 
-def firstSync():
+        addedBy = track['added_by']['id']
+        addedAt = track['added_at']
+        url = track['track']['artists'][0]['external_urls']['spotify']
+        artistName = track['track']['artists'][0]['name']
+        trackName = track['track']['name']
+        spotifyId = track['track']['id']
+
+        print("Added By: {}".format(addedBy))
+        print("Added At: {}".format(addedAt))
+        print("Url: {}".format(url))
+        print('ArtistName: {}'.format(artistName))
+        print('trackName: {}'.format(trackName))
+        print('SpotifyId: {}'.format(spotifyId))
+        print('\n')
+
+
+
+def syncAndInsert():
     '''
     Download all information for the first time Created
     and insert to database
@@ -88,8 +100,45 @@ def firstSync():
         trackName = track['track']['name']
         spotifyId = track['track']['id']
 
-        insertTrack(addedBy, addedAt, url, artistName, trackName, spotifyId)
-        pprint.pprint(track)
-        logger.debug("addedBy: %s addedAt: %s artistName: %s trackName: %s\n URL: %s", addedBy, addedAt, artistName, trackName, url)
+        # check if track exists in database
+        queryIdFromDatabase = c.execute('select spotifyId from playlist where spotifyId=(?);', [spotifyId])
 
-firstSync()
+        # if it doesn't, insert it
+        if queryIdFromDatabase is None:
+            #print(queryIdFromDatabase.fetchone()[0])
+            insertTrack(addedBy, addedAt, url, artistName, trackName, spotifyId, 0)
+            logger.info('Inserted Track - Artist: {} - trackname: {}'.format(artistName, trackName))
+
+###########################################################################
+
+def getMostRecentRon():
+    ''' query database for most recently added song'''
+    ronDatabaseObject = c.execute('select * from playlist where addedBy="ronlipke" order by addedAt desc limit 1')
+    ronDatabaseList = ronDatabaseObject.fetchone()
+    return ronDatabaseList
+
+def getMostRecentAdam():
+        ''' query database for most recently added song'''
+        adam = c.execute('select * from playlist where addedBy="nonstopflights" order by addedAt desc limit 1')
+        adamDatabaseList = adamDatabaseObject.fetchone()
+        return adamDatabaseList
+
+###########################################################################
+''' Connect to Database '''
+conn = sqlite3.connect(dbName)
+c = conn.cursor()
+logger.debug('-- Logging into Database')
+###########################################################################
+''' Create database if not there '''
+createTables()
+logger.debug("Check for Database")
+###########################################################################
+
+#debug__pullJson(resultsObject)
+syncAndInsert()
+#getMostRecentRon()
+
+conn.close()
+# ----> pull json
+# ----> sort by date, user -> get spotifyId
+# ----> write newest to json file
