4e2e8459533a3d0d75e02fbd84fdd53c30d95946
Adam Schoonover <adamschoonover@Elchert-Family-MBP.local>
2017-07-31 23:10:48 -0400

    updates

--- Scripts/spotify/main.py
+++ Scripts/spotify/main.py
@@ -19,10 +19,6 @@ log.setFormatter(formatter)
 logger.addHandler(log)
 ########################################################################
 
-SPOTIPY_CLIENT_ID='70be013bbce941b19cc1b0c22d66c6c3'
-SPOTIPY_CLIENT_SECRET='7e4bfe34feb942cc8e1f92d7c1293e18'
-SPOTIPY_REDIRECT_URI='http://sevone.elchert.net'
-
 client_credentials_manager = SpotifyClientCredentials()
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
