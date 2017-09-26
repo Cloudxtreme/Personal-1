76cb9eedc94cb07a07d4b1f93ef12ea4e415d917
Adam Schoonover <aschoonover@sevone.com>
2017-09-26 15:50:48 -0400

    updates

--- Server/Cronjobs/btrfsCheck.sh
+++ Server/Cronjobs/btrfsCheck.sh
@@ -5,12 +5,19 @@
 #
 # grep's for the 0.
 
+NOW=$(date +"%m-%d-%Y")
+LOGDATE=$(date +"%m-%d-%Y %H:%M:%S")
+
+DIR="/home/aelchert/Dropbox/Backup"
+LOGFILE="/home/aelchert/Dropbox/Logs/cronLog.txt"
+
 sudo /bin/btrfs device stats /mnt/NAS/ | grep -vE ' 0$'
 
 # If something is returned, send email
-
 if [$? == 0]; then
   echo "There was a response"
+  echo -e "++ [btrfsCheck.sh] - $LOGDATE - btrfs health check - [FAILED]" >> $LOGFILE
 else
   echo "No response"
+  echo -e "-- [btrfsCheck.sh] - $LOGDATE - btrfs health check - [OK]" >> $LOGFILE
 fi
