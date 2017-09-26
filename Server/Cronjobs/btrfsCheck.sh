9327854fbebc6fadf2f1e0d20c7f67c311248595
Adam Schoonover <aschoonover@sevone.com>
2017-09-26 15:47:15 -0400

    updates

---
+++ Server/Cronjobs/btrfsCheck.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Run BTRFS system check, returns something like
+#[/dev/sdc].write_io_errs   0
+#
+# grep's for the 0.
+
+/bin/btrfs deviec stats /mnt/NAS/ | grep -vE ' 0$'
+
+# If something is returned, send email
+
+if [$? == 0]; then
+  echo "There was a response"
+else
+  echo "No response"
