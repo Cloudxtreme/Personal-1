147bd313b88827902adb0e92e1f6cb84ce98565d
Adam Schoonover <aschoonover@sevone.com>
2018-01-09 18:50:14 -0500

    updates

--- Server/Cronjobs/deferredData/borg/borgCount.sh
+++ Server/Cronjobs/deferredData/borg/borgCount.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+##############
+# Script will output a count of all files borg backs up in the
+# most current backup
+#
+# To be used in a deferred data scirpt to SevOne
+#
+# v.1 Initial commit
+###############
+
 backupDirectory="/mnt/Backups"
 getLatestBackupDate=$(sudo borg list $backupDirectory | awk {'print $1;'} | tail -n 1)
 outputBorgCount=$(sudo borg list $backupDirectory::$getLatestBackupDate | wc -l | tee  borgCount.txt)
@@ -7,6 +16,12 @@ outputBorgCount=$(sudo borg list $backupDirectory::$getLatestBackupDate | wc -l 
 
 echo "Last backup date: "
 echo $getLatestBackupDate
+printf "\n"
 
 echo "File Count: "
 echo $outputBorgCount
+if [ $? -eq 0 ]; do
+  echo "File Created Successfully"
+else
+  echo $!
+  echo "File Not Created Successfully"
