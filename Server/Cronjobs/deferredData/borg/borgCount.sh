771f1828dbcd77460d57e502a08e7aa5938236c0
Adam Schoonover <aschoonover@sevone.com>
2018-01-09 18:37:33 -0500

    updates

--- Server/Cronjobs/deferredData/borg/borgCount.sh
+++ Server/Cronjobs/deferredData/borg/borgCount.sh
@@ -3,3 +3,5 @@
 backupDirectory="/mnt/Backups"
 getLatestBackupDate="borg list $backupDirectory | awk {'print $1;'} | tail -n 1"
 outputBorgCount="borg list $backupDirectory::$getLatestBackupDate | wc -l > borgCount.txt"
+
+outputBorgCount
