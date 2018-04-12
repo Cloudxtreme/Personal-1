9e290e1df69952baf64e131685b0e3c40a4bcd4e
Adam Schoonover <aschoonover@sevone.com>
2018-04-11 23:05:19 -0400

    updates

--- Server/Cronjobs/deferredData/allergy/allergies.py
+++ Server/Cronjobs/deferredData/allergy/allergies.py
@@ -357,6 +357,7 @@ def insertData(questionsDict):
                   "timestamp": currentEpochtime
                 }
               ]
+          print(dataDict)
 
     try:
       r = requests.post(post_indicatorData, headers=header,json=dataDict)
