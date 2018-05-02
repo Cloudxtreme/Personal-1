a96de120bf221c8e5bc6f7336b68d70c972a873c
Adam Elchert <adam@elchert.net>
2018-05-02 18:03:38 -0400

    updates

--- Server/Cronjobs/nginxBackup.py
+++ Server/Cronjobs/nginxBackup.py
@@ -1,34 +1,75 @@
+#!/usr/bin/python3.6
+
 import filecmp,shutil,os
 
-endDIR="/home/aelchert/Git/Personal/Backups/Nginx"
-startDIR="/tmp/nginxTempDIR"
+def debugPrint(x):
+    if __name__ == '__main__':
+        print(x)
+'''
+PURPOSE: Keep a backup of active nginx conf files
 
-compare = filecmp.dircmp(startDIR, endDIR)
+1. ssh into nginx vm
+2. pull nginx conf files to a local tmp directory
+3. compare conf files in tmp directory to backup directory
+4. copy over new files
+5. delete files in tmp directory
 
-# see if temp directory exists. If not, create it. If so, delete and recreate it
-if os.path.exists(startDIR):
-	shutil.rmtree(startDIR)
-	os.mkdir(startDIR)
+filecmp url: https://docs.python.org/2/library/filecmp.html
+
+v.2 - 5/2/18 - Script wasn't working. Cleaned it up and added functionality to clean it up the 
+backup dir of non-used files.
+'''
+
+tmpDir="/tmp/nginxTempDIR"
+backupDir="/home/aelchert/Git/Personal/Backups/Nginx/"
+
+# See if tmp folder exists. If not, create it. 
+# If it does already, delete and recreate it to start fresh
+
+if os.path.exists(tmpDir):
+    debugPrint("Folder Existed - Deleting")
+    shutil.rmtree(tmpDir)
+    debugPrint("Folder Created")
+    os.mkdir(tmpDir)
 else:
-	os.mkdir(startDIR)
+    debugPrint("Creating Directory")
+    os.mkdir(tmpDir)
 
-# pulling the files to the temp dir	
-os.system('scp -q aelchert@10.0.0.57:/etc/nginx/conf.d/*.conf /home/aelchert/Git/Personal/Backups/Nginx/')
+# Pull files from nginx 
+os.system('scp -q aelchert@10.0.0.57:/etc/nginx/conf.d/* {}'.format(tmpDir))
 
-# if no new files, then exit
-if len(compare.left_only) == 0:
-    print("No changes")
+# Compares tmp and backup directory
+compare = filecmp.dircmp(tmpDir, backupDir)
+
+# takes the comparison and tells you if the backup directory has files the tmp does not
+filesToCleanUp = compare.right_only
+
+# these are files that are in the tmp directory only
+filesToBackup = compare.left_only
+
+# Clean up backup directory of files not in current use
+# compares files in tmp directory to backup dir
+for file in filesToCleanUp:
+    debugPrint("Files exist")
+    debugPrint("Removing File: {}".format(file))
+    os.remove(backupDir + file)
+
+# if no new files, remove tmp directory and then exit
+if len(compare.diff_files) == 0:
+    debugPrint("No different files in tmp and backup directory")
+    # No new files, so delete tmp directory
+    debugPrint("Removing {}".format(tmpDir))
+    shutil.rmtree(tmpDir)
     exit()
-
 else:
+    for file in filesToBackup:
+        start = tmpDir + "/" + file
+        end = backupDir + "/" + file
+        shutil.copyfile(start, end)
 
-    for files in compare.left_only:
-        start = startDIR + "/" + files
-        end = endDIR + "/" + files
-        shutil.copyfile(start,end)
-
-        if __name__ == '__main__':
-            print("Copying file => {}".format(files))
+        debugPrint(print("Copying file => {}".format(file)))
+            
 
 # remove temp directory
-shutil.rmtree(startDIR)
+debugPrint("Removing {}".format(tmpDir))
+shutil.rmtree(tmpDir)
