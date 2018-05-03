7c3dbfcb49729679cae342525ace993df89f8deb
Adam Elchert <adam@elchert.net>
2018-05-02 21:17:05 -0400

    updates

--- Server/Cronjobs/btrfsCheck.sh
+++ Server/Cronjobs/btrfsCheck.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# v.2 - 5/2/18 - Fixed logging 
+
 # Run BTRFS system check, returns something like
 #[/dev/sdc].write_io_errs   0
 #
 # grep's for the 0.
+
+
 
 NOW=$(date +"%m-%d-%Y")
 LOGDATE=$(date +"%m-%d-%Y %H:%M:%S")
@@ -11,12 +15,16 @@ LOGDATE=$(date +"%m-%d-%Y %H:%M:%S")
 DIR="/home/aelchert/Dropbox/Backup"
 LOGFILE="/home/aelchert/Dropbox/Logs/cronLog.txt"
 
-sudo /bin/btrfs device stats /mnt/NAS/ | grep -vE ' 0$'
+/bin/btrfs device stats /mnt/NAS/ | grep -vE ' 0$'
 
-if [ $? == 0 ]; then
+if [ $? -eq 0 ]; then
   echo "There was a response"
-  echo -e "++ [btrfsCheck.sh] - $LOGDATE - btrfs health check - [FAILED]" >> $LOGFILE
+  echo -e "++ [btrfsCheck.sh] - $LOGDATE - btrfs health check - [[ FAILED ]]" >> $LOGFILE
+elif [ $? -eq 1 ]; then
+    echo "Health Check OK"
+    echo -e "-- [btrfsCheck.sh] - $LOGDATE - btrfs health check - [[ OK ]]" >> $LOGFILE
 else
-  echo "No response"
-  echo -e "-- [btrfsCheck.sh] - $LOGDATE - btrfs health check - [OK]" >> $LOGFILE
+  echo "Weird response"
+  echo $?
+  echo -e "-- [btrfsCheck.sh] - $LOGDATE - btrfs health check - [[ Weird response ]]" >> $LOGFILE
 fi
